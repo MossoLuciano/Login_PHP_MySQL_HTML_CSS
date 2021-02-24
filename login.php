f436a5d32bc8f0fb3f864631445a7a8770c3a130
MossoLuciano <luchomosso30@gmail.com>
2021-02-24 16:31:05 -0300

    Cambio a un tamaño menor los botones del login

--- login.php
+++ login.php
@@ -6,6 +6,7 @@
     if(isset($_POST['ingresar'])){
         $email = $_POST['email'];
         $clave = $_POST['clave'];
+        
         VerificarUsuario($email, $clave);
     }
 ?>
@@ -15,12 +16,12 @@
 </div>
 
 <div class="login">
-    <form action="operaciones.php" method="POST" class="formularioLogin">
+    <form action="#" method="POST" class="formularioLogin">
         <input type="email" name="email" class="email" placeholder="Email"><br><br>
         <input type="password" name="clave" class="clave" placeholder="Password"><br><br>
-        <div class="botones">
-            <input type="submit" value="Ingresar" name="ingresar" class="btn btn-xs btn-block btn-primary">
-            <input type="submit" value="Cancelar" name="cancelar" class="btn btn-xs btn-block btn-primary"><br><br>
+        
+        <input type="submit" value="Ingresar" name="ingresar" class="btn btn-sm btn-block btn-primary" id="ingresar">
+        <input type="submit" value="Cancelar" name="cancelar" class="btn btn-sm btn-block btn-primary"><br><br>
         </div><br>
     </form>
 </div>
