d1b42455e313193bcfacc79f65e9d595e1f5957b
MossoLuciano <luchomosso30@gmail.com>
2021-02-24 09:57:58 -0300

    Reacomodamiento de botones del login

--- login.php
+++ login.php
@@ -18,12 +18,13 @@
     <form action="operaciones.php" method="POST" class="formularioLogin">
         <input type="email" name="email" class="email" placeholder="Email"><br><br>
         <input type="password" name="clave" class="clave" placeholder="Password"><br><br>
+        <div class="botones">
+            <input type="submit" value="Ingresar" name="ingresar" class="btn btn-xs btn-block btn-primary">
+            <input type="submit" value="Cancelar" name="cancelar" class="btn btn-xs btn-block btn-primary"><br><br>
+        </div><br>
     </form>
 </div>
-<div class="botones">
-    <input type="submit" value="Ingresar" name="ingresar" class="btn btn-xs btn-block btn-primary">
-    <input type="submit" value="Cancelar" name="cancelar" class="btn btn-xs btn-block btn-primary"><br><br>
-</div><br>
+
 <div class="vinculoRegistro">
     <a href="registro.php">Registrarme</a>
 </div><br><br><br><br>
