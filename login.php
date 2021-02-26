05838f7d3aaeda916defb5370863b8751d9255be
MossoLuciano <luchomosso30@gmail.com>
2021-02-26 19:46:08 -0300

    Terminacion de login.php

--- login.php
+++ login.php
@@ -10,18 +10,20 @@
         VerificarUsuario($email, $clave);
     }
 ?>
-
-<div class="login">
-    <h2 class="tituloLogin">Login</h2><br>
-    <form action="#" method="POST" id="formularioLogin">
-        <input type="email" name="email" class="email" placeholder="Email"><br><br>
-        <input type="password" name="clave" class="clave" placeholder="Password"><br><br>
-        <input type="submit" value="Ingresar" name="ingresar" class="btn btn-sm  btn-primary" id="ingresar">
-        <input type="submit" value="Cancelar" name="cancelar" class="btn btn-sm  btn-primary"><br>
-        </div><br>
-    </form>
-</div>
-
-<div class="vinculoRegistro">
+<div class="mainLogin">
+    <div class="login">
+        <h2 class="tituloLogin">Login</h2><br>
+        <form action="#" method="POST" id="formularioLogin">
+            <input type="email" name="email" class="email" placeholder="Email"><br><br>
+            <input type="password" name="clave" class="clave" placeholder="Password"><br><br>
+            <input type="submit" value="Ingresar" name="ingresar" class="btn btn-sm  btn-primary" id="ingresar">
+            <input type="submit" value="Cancelar" name="cancelar" class="btn btn-sm  btn-primary"><br>
+        </form>
+    </div><br>
+    <div class="vinculoRegistro">
     <a href="registro.php">Registrarme</a>
-</div><br>
+    </div><br>
+</div>
+   
+
+
