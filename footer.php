a76d99ca4fd0e2f7ac9cc99cdc04343e8e07134a
MossoLuciano <luchomosso30@gmail.com>
2021-02-26 19:14:14 -0300

    Terminacion de index.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div class="footer">
-    <h3>Nuevo Banco Max</h3>
+    <h3>Nuevo Banco Max</h3><br>
     <p><strong>03404-514785 / 03404-578912</strong</p>
     <p><strong>Dirección: Mitre 123</strong></p>
     <p><strong>Email: bancomax@gmail.com</strong></p>
